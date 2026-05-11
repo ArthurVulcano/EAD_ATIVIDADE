0b274240f24b075445630f4a870c9acfab88293e
ARTHUR LIMA SANTOS <arthurlimasantos2945@gmail.com>
2026-05-11 08:28:19 -0300

    Update BuscadorDeNota.java

--- BuscadorDeNota.java
+++ BuscadorDeNota.java
@@ -3,7 +3,7 @@ package Lista_ead;
 import java.util.Scanner;
 
 public class BuscadorDeNota {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sm = new Scanner(System.in);
         Buscador_de_nota.mordor mordor = new Buscador_de_nota.mordor();
         System.out.println("Digite 8 notas quaisquer!");
